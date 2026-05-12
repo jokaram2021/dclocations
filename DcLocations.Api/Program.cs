cbde2648105be30eca67953f40768a316d128ae1
Karam <jokaram@chapman.edu>
2026-05-12 16:50:10 -0700

    applied some fixes

--- DcLocations.Api/Program.cs
+++ DcLocations.Api/Program.cs
@@ -8,6 +8,8 @@ using Microsoft.OpenApi.Models;
 
 using System.Text;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 
@@ -20,49 +22,7 @@ builder.Services.AddEndpointsApiExplorer();
 
 
 
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc(
-        "v1",
-        new OpenApiInfo
-        {
-            Title = "DC Locations API",
-            Version = "v1"
-        }
-    );
-
-    options.AddSecurityDefinition(
-        "Bearer",
-        new OpenApiSecurityScheme
-        {
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            Scheme = "bearer",
-            BearerFormat = "JWT",
-            In = ParameterLocation.Header,
-            Description = "Enter JWT Bearer token"
-        }
-    );
-
-    options.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference =
-                        new OpenApiReference
-                        {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = "Bearer"
-                        }
-                },
-
-                new string[] {}
-            }
-        }
-    );
-});
+builder.Services.AddSwaggerGen();
 
 
 
@@ -123,12 +83,11 @@ var app = builder.Build();
 
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
+app.UseSwagger();
 
-    app.UseSwaggerUI();
-}
+
+
+app.UseSwaggerUI();
 
 
 
